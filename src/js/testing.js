c6e94810aaeba185c47febda8323d730e8ce4f33
Valentin <vlntnkzrv@gmail.com>
2021-03-10 17:17:46 +0100

    slider section update

---
+++ src/js/testing.js
@@ -0,0 +1,70 @@
+/* let number = 7;
+const pi  = 3.14; */
+
+/* JS data types:  */
+//number
+//string - "", '', 
+//boolings - true/false 
+//null
+//undefined 
+/* let onj = {
+    name:'apple'
+    color:'green'
+    weight:'200'
+} */
+
+//Symbol
+
+/* alert(12345); */
+
+/* console.log(number); */
+/* let answer = confirm( "are u 18 years old?") ;
+console.log(answer); */
+
+/* let answer = prompt( "are u 18 years old?", "");
+console.log(answer); */
+
+/* console.log(4+"test"); */
+
+/* let isChecked = true,
+    isClose = false; 
+
+/* console.log(isChecked && isClose);  */
+
+/* console.log(isChecked || isClose);  */ 
+
+/* if (2*6 == 8*1) {
+    console.log('vernoe virazhenie')
+} else[
+    console.log('oshibka')
+] */
+
+/* let answer = confirm('are u 18 years old?');
+if (answer) {
+    console.log('prohodite')
+}   else{
+    console.log('ne prohodite')
+}
+
+const num = 50; 
+
+if (num < 49) {
+    console.log('Nepravilno')
+} else if (num>100) {
+    console.log('Mnogo')
+} else{
+    console.log('verno')
+} */
+
+/* for(let i = 1; i < 8; i++) {
+    console.log(i);
+} */
+
+/* function logging (a, b){
+    console.log(a + b)
+}
+
+logging(3 ,5 );  */
+
+
+
