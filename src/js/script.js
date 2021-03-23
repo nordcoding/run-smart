b017087be9d31403300baee333e0047409f78f3f
Valentin Home <vlntnkzrv@gmail.com>
2021-03-23 18:19:04 +0100

    animation finall update

--- src/js/script.js
+++ src/js/script.js
@@ -159,11 +159,14 @@ $(document).ready(function(){
 	});
 
 	//Smooth scroll settings 
-	$("a[href^='#']").click(function(){
+	$("a[href^='#up']").click(function(){
 		const _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
 	});
+
+	new WOW().init();
+	
 });
 
 /* const slider = tns({
