d7c025eb6e0b61ca64502622b29c50afd428716a
Valentin <vlntnkzrv@gmail.com>
2021-04-14 19:13:29 +0200

    final update

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'vlntnkzrv@gmail.com';                 // Наш логин
-$mail->Password = 'Lowj03gegdj9_21!';                           // Наш пароль от ящика
+$mail->Password = 'password';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
