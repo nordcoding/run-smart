7c6eadff222c275d07e506ab822575dde4c74d4c
Valentin <vlntnkzrv@gmail.com>
2021-04-19 14:01:46 +0200

    responsive ver 1.0 update

--- gulpfile.js
+++ gulpfile.js
@@ -65,4 +65,4 @@ gulp.task('images',function(){
     .pipe(gulp.dest("dist/img"));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles','html','scripts', 'fonts', 'icons','mailer', 'images' ));
+gulp.task('default', gulp.parallel('watch', 'server', 'html', 'styles','scripts', 'fonts', 'icons','mailer', 'images' ));
